0bbe0ad857e027bfefafbae248e1171b05340deb
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-08-27 18:02:04 +0800

    build docs

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: 'src',
-            output: 'dist',
+            output: 'docs',
             pathPrefix: '/weather/',
         },
     };
